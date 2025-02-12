a6837962b006f14f4272ba7ad317781a6918eeb3
Swapnil Tiwari <swapniltiwari43@gmail.com>
2025-02-13 00:52:31 +0100

    Time: 174 ms (5.53%) | Memory: 59.9 MB (5.53%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxAncestorDiff = function(root) {
+    let maxDiff = 0;
+    const dfs = (currNode) => {
+        if (currNode === null) {
+            return {
+                min: null,
+                max: null
+            };
+        }
+        
+        if (isLeafNode(currNode)) {
+            return {
+                min: currNode.val,
+                max: currNode.val
+            };
+        }
+        
+        const leftBounds = dfs(currNode.left);
+        const rightBounds = dfs(currNode.right);
+        console.log("Node", currNode.val)
+        console.log("leftBounds", leftBounds)
+        console.log("rightBounds", rightBounds)
+        const maxDiffLeft = leftBounds.min !== null ? getMaxDiff(currNode.val, leftBounds) : 0;
+        const maxDiffRight = rightBounds.min !== null ? getMaxDiff(currNode.val, rightBounds) : 0;
+        const currMaxDiff =  Math.max(maxDiffLeft, maxDiffRight);
+        console.log("currMaxDiff", currMaxDiff)
+        console.log("maxDiff", maxDiff)
+        maxDiff = Math.max(currMaxDiff, maxDiff)
+        console.log("newMaxDiff", maxDiff)
+        console.log("min", Math.min(leftBounds.min || Infinity, rightBounds.min))
+        console.log("max", Math.max(leftBounds.max || -Infinity, rightBounds.max))
+        return {
+            min: Math.min(leftBounds.min === null ? Infinity : leftBounds.min, rightBounds.min === null ? Infinity : rightBounds.min, currNode.val),
+            max: Math.max(leftBounds.max === null ? -Infinity : leftBounds.max,  rightBounds.max === null ? -Infinity : rightBounds.max, currNode.val),
+        } 
+    }
+    
+    dfs(root, Infinity, -Infinity);
+    return maxDiff;
+};
+
+const getMaxDiff = (val, bounds) => Math.max(Math.abs(val - bounds.min), Math.abs(val - bounds.max))
+
+const isLeafNode = (node) => node.left === null && node.right === null;
