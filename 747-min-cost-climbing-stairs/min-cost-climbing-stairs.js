434a5d9502c2c1b54c4565afbc28555443f8c336
Swapnil Tiwari <swapniltiwari43@gmail.com>
2025-02-15 21:25:42 +0100

    Time: 2 ms (55.73%) | Memory: 49.5 MB (96.01%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -4,11 +4,6 @@
  */
 var minCostClimbingStairs = function(cost) {
     const floorCount = cost.length;
-    // minCost[i] === Minimum cost to get to floor i
-    const minCost = new Array(floorCount+1);
-    
-    minCost[0] = 0;
-    minCost[1] = 0;
     let minCosrCurr;
     let minCostPrev = 0;
     let minCostPrev2 = 0;
