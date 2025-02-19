4ddbf1777a5769638fc465a0764d8f66bd015a4b
Swapnil Tiwari <swapniltiwari43@gmail.com>
2025-02-19 23:01:11 +0100

    Time: 19 ms (17.25%) | Memory: 62.4 MB (5.01%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    const visited = new Set();
+    
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let maxArea = 0;
+    
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            const currElement = grid[row][col];
+            const key = `${row}-${col}`;
+            
+            if (currElement === 1 && !visited.has(key)) {
+                const stack = [key];
+                let currArea = 0;
+
+                while (stack.length) {
+                    // Element to explore from
+                    const currKey = stack.shift();
+                    let [currRow, currCol] = currKey.split('-');
+                    currRow = parseInt(currRow);
+                    currCol = parseInt(currCol);
+                    if (!visited.has(currKey)) {
+                        visited.add(currKey);
+                        currArea++
+                        
+                        const leftKey = `${currRow}-${currCol-1}`;
+                        const rightKey = `${currRow}-${currCol+1}`;
+                        const topKey = `${currRow-1}-${currCol}`;
+                        const bottomKey = `${currRow+1}-${currCol}`;
+
+                        if (currCol > 0 && grid[currRow][currCol-1] === 1 && !visited.has(leftKey)) {
+                            stack.push(leftKey);
+                        }
+                        if (currCol < cols-1 && grid[currRow][currCol+1] === 1 && !visited.has(rightKey)) {
+                            stack.push(rightKey);
+                        }
+                        if (currRow > 0 && grid[currRow-1][currCol] === 1 && !visited.has(topKey)) {
+                            stack.push(topKey);
+                        }
+                        if (currRow < rows -1 && grid[currRow+1][currCol] === 1 && !visited.has(bottomKey)) {
+                            stack.push(bottomKey);
+                        }
+                    }
+                }
+                maxArea = Math.max(currArea, maxArea)
+            }
+            
+        }
+    }
+    return maxArea;
+};
