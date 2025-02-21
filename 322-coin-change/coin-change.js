b5c7a99b5bc65c838f9c959d9b4b9e7ab9fee931
Swapnil Tiwari <swapniltiwari43@gmail.com>
2025-02-21 11:22:55 +0100

    Time: 102 ms (21.54%) | Memory: 61.8 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -4,18 +4,25 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    if (amount === 0) return 0;
-
-    const dp = new Array(amount + 1).fill(Infinity);
-    dp[0] = 0;  // Base case: 0 coins are needed for amount 0
-
-    for (let currAmount = 1; currAmount <= amount; currAmount++) {
+    const dp = {};
+    const dfs = (rem) => {
+        if (rem < 0) {
+            return -1
+        }
+        if (rem === 0) {
+            return 0;
+        }
+        let minCost = Infinity;
         for (let coin of coins) {
-            if (currAmount - coin >= 0) {
-                dp[currAmount] = Math.min(dp[currAmount], dp[currAmount - coin] + 1);
+            const result = dp[rem-coin] || dfs(rem-coin);
+            if (result !== -1) {
+                minCost = Math.min(minCost, result);
             }
         }
+        const answer = minCost === Infinity ? -1 : minCost + 1;
+        dp[rem] = answer;
+        return answer;
     }
 
-    return dp[amount] === Infinity ? -1 : dp[amount];
+    return dfs(amount)
 };
